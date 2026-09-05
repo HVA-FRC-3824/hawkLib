89147e4b45d42f989fe30bd0fad5d2a3e1f2a681
Jackson Case <casejack100@protonmail.com>
2026-09-05 18:30:31 -0400

    Update license header

--- hardware/motor/rev/MotorIOSparkMax.java
+++ hardware/motor/rev/MotorIOSparkMax.java
@@ -2,7 +2,7 @@
 // http://github.com/HVA-FRC-3824
 //
 // Use of this source code is governed by an MIT-style license that can be found in the LICENSE file at
-// the root directory of this project.
+// the root directory of this project. Some code may be governed by other licenses which can be found in the "/External Licenses" directory.
 
 package frc.shared.hardware.motor.rev;
 
